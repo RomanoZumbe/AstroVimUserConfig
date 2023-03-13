7a382b5221a9475f0aecf422aef3b223cea80424
Romano Zumbe <rzumbe@gmail.com>
2023-03-13 06:22:06 +0100

    first adjustments

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,5 +1,18 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      textsubjects = {
+        enable = true,
+        prev_selection = ',', -- (Optional) keymap to select the previous selection
+        keymaps = {
+          ['.'] = 'textsubjects-smart',
+          [';'] = 'textsubjects-container-outer',
+          ['i;'] = 'textsubjects-container-inner',
+        },
+      },
+    }
+  end,
   opts = {
     -- ensure_installed = { "lua" },
   },
