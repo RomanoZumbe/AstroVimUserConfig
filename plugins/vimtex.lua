2acb04d50864308db8b7dbd31056a5ff847ac1e1
Romano Zumbe <rzumbe@gmail.com>
2023-04-16 11:41:33 +0200

    stuff

--- plugins/vimtex.lua
+++ plugins/vimtex.lua
@@ -2,8 +2,10 @@ return {
   "lervag/vimtex",
   lazy = false,
   config = function()
-    vim.g.vimtex_view_general_viewer = 'C:/Users/MSI/AppData/Local/SumatraPDF/SumatraPDF'
-    vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
-    vim.g.vimtex_view_general_options_latexmk = '-reuse-instance'
+    vim.g.vimtex_view_method = 'zathura'
+    -- vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
+    -- vim.g.vimtex_view_general_viewer = 'zathura'
+    -- vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+    -- vim.g.vimtex_view_general_options_latexmk = '-reuse-instance'
   end
 }
